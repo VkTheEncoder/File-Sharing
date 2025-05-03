4b4af77ba4d331c7d6d8cd97811b1a5f7de11f91
VkTheEncoder <158612790+VkTheEncoder@users.noreply.github.com>
2025-05-03 14:19:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -88,5 +88,5 @@ URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Ask Doubt on telegram @KingVJ0
     
